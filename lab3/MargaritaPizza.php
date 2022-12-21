7cd917907c6f7f52c0417ac9c44f772e4ac8049c
lamrenik <belaev578@gmail.com>
2022-12-21 17:10:25 +0300

    refactor

--- lab3/MargaritaPizza.php
+++ lab3/MargaritaPizza.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../lab2/vendor/autoload.php';
+require_once __DIR__ . '/vendor/pavel/lw2/vendor/autoload.php';
 use Pavel\Lw2\Pizza;
 
 class MargaritaPizza extends Pizza
